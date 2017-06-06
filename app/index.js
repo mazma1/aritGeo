d574bd4f5fc50fdd66c3a41a2fa9dc76ffca57e7
Mary Mazi <mazi.mary.o@gmail.com>
2017-06-06 14:56:33 +0100

    Refactor my app code to have V1.2

--- app/index.js
+++ app/index.js
@@ -3,46 +3,36 @@
 module.exports = {
 
     aritGeo: (arr) => {
+
+        let diff = arr[1] - arr[0];
+        let ratio = arr[1] / arr[0];
+
+        let arithm = true;
+        let geo = true;
+
         if ((typeof arr === 'object') && (arr instanceof Array)) {
-            if (arr.length == 0) {    //An empty array
+            for(let i = 0; i < arr.length - 1; i++) {
+                if( arr[i + 1] - arr[i] !== diff ) {
+                    arithm = false;
+                }        
+                if(arr[i + 1] / ratio !== arr[i]) {
+                    geo = false;
+                }        
+            }
+
+            if (arr.length == 0) {
                 return 0;
-            } 
-            if (isArithmetic(arr) === true) {
-                return 'Arithmetic';
-            } 
-            else if (isGeometric(arr) === true) {
+            }
+            else if(arithm === true) {
+                return "Arithmetic";
+            }        
+            else if(geo === true) {
                 return "Geometric";
-            } 
+            }      
             else {
                 return -1;
-            }   
-        }
-
-        // Function that tests if an array is Arithmetic
-        function isArithmetic(arr) {
-            let diff = arr[1] - arr[0];
-            let arithm = true;
-
-            for (let i = 0; i < arr.length - 1; i++) {
-                if ((arr[i+1] - arr[i]) !== diff) {
-                arithm = false;
-                }
             }
-            return arithm;
-        }
-
-        // Function that tests if an array is Geometric
-        function isGeometric (arr) {
-            let ratio = arr[1]/arr[0];
-            let geo = true;
-            
-            for (let j = 0; j < arr.length - 1; j++) {
-                if ((arr[j+1]/arr[j]) !== ratio) {
-                geo = false;
-                } 
-            }
-            return geo;
-        }
+        } 
     }
         
 }    
